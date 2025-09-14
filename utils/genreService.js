547969714d090b907e323bdcc868f5b15f9eeb03
Owethu927 <owethusilela@gmail.com>
2025-09-14 19:00:48 +0300

    mapping through id and names

---
+++ utils/genreService.js
@@ -0,0 +1,12 @@
+import { genres } from "../data";
+
+/**
+ * @param {object}
+ * @param {genres}
+ */
+
+export function genreServices() {
+  let genre = Object.entries(genres).map(([key, values]) => {
+    return genre;
+  });
+}
--- views/createGrid.js
+++ views/createGrid.js
@@ -1,6 +1,9 @@
 import { createPodastCard } from "./components/createPodastCard.js";
 import { createModal } from "./components/createModal.js";
 
-export function createGrid() {
+export function createGrid(podcasts) {
   const container = document.getElementById("container");
+  podcasts.forEach((container) => {
+    c;
+  });
 }
