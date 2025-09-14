f84eb22c1d75db22b8a06cd667abfa59b94dbb23
Owethu927 <owethusilela@gmail.com>
2025-09-14 11:56:39 +0300

    creayed a fuction the will create grids

---
+++ views/createGrid.js
@@ -0,0 +1,6 @@
+import { createPodastCard } from "./components/createPodastCard.js";
+import { createModal } from "./components/createModal.js";
+
+export function createGrid() {
+  const container = document.getElementById("container");
+}
