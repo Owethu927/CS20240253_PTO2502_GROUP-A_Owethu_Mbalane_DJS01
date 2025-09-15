99c6dde3d2ad94728e5e3f7e354551a70fd691f1
Owethu927 <owethusilela@gmail.com>
2025-09-15 21:51:42 +0300

    restarting my create podcast by creating a div

--- components/createPodcastCard.js
+++ components/createPodcastCard.js
@@ -1,15 +1,4 @@
-import { podcasts } from "../data";
-
-function createPodastCardGrid(podcast) {
-  const card = document.createElement("div");
-  card.innerHTML = `
-    <img>
-    <h3>${podcast.title}</h3>
-    <p>${podcast.seasons}</p>
-    <p>Genre: ${podcast.genre.join(",")}</p>`;
-
-  card.addEventListener("click", () => {
-    aleart(`clicked on the podcast:${podcast.title}`);
-  });
-  return card;
+function createPodastCardGrid(podcast, onClick) {
+  // c
+  const createCard = document.createElement("div");
 }
