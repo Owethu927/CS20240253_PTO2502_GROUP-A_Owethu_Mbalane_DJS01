b0223f79fa4d6269c1401d5268811656f3c25b4b
Owethu927 <owethusilela@gmail.com>
2025-09-15 22:18:45 +0300

    Added the inner html and map through each erray in genre

--- components/createPodcastCard.js
+++ components/createPodcastCard.js
@@ -1,4 +1,18 @@
 function createPodastCardGrid(podcast, onClick) {
-  // c
+  // Creating a div
   const createCard = document.createElement("div");
+  createCard.className("podcast-card");
+  // adding the inner HTML using template literals
+  createCard.innerHTML = `
+   <img class="podcast-cover" src="${podcast.image}" alt="${podcast.title}">
+   <div>
+    <h3>${podcast.title}</h3>
+    <p><strong>Seasons:</strong>${
+      podcast.seasons === 1 ? "1 season" : podcast.seasons + "seasons"
+    }</p>
+    <div class= "podcast-genre">
+    ${podcast.genre.map((g) => `<span class="genre-tag">${g}</span>`.join(""))}
+    </div>
+    <p><strong>Last Updated</strong>${podcast.lastUpdated}</p>
+    </div>`;
 }
