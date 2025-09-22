f44ed0be629b49b90ced268b048566cc7bfb259d
Owethu927 <owethusilela@gmail.com>
2025-09-22 11:54:14 +0300

    change imports and addes css

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import { podcasts } from "./data.js";
 import { createModal } from "./components/createModal.js";
 import { createGrid } from "./views/createGrid.js";
 
+/**
+ * Initializes the podcast application.
+ *
+ * @principle SRP - Only responsible for application startup logic like event binding and rendering initial grid.
+ */
 function init() {
-  document.getElementById("myModal");
-  document.addEventListener("click", createModal.close);
+  document
+    .getElementById("closeModal")
+    .addEventListener("click", createModal.close);
   const grid = createGrid();
   grid.render(podcasts);
 }
