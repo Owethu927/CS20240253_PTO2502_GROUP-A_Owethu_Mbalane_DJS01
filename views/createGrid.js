7adf30116e9b1c8577f1bce944d12e0008791edf
Owethu927 <owethusilela@gmail.com>
2025-09-22 11:40:43 +0300

    created the podcast cards grids

--- views/createGrid.js
+++ views/createGrid.js
@@ -9,4 +9,20 @@ import { createModal } from "./components/createModal.js";
 
 export const createGrid = () => {
   const container = document.getElementById("podcastGrid");
+
+  return {
+    /**
+     * Render a list of podcast cards into grids
+     *
+     * @param {objects[]} - array of podcast objects
+     */
+
+    render(podcastList) {
+      container.innerHTML = "";
+      podcastList.forEach((p) => {
+        const card = createPodastCard(p, createModal.open);
+        container.appendChild(card);
+      });
+    },
+  };
 };
